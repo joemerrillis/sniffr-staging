34fa370ffe39372ebc9becfc1bab632c8723b985
joemerrillis <joemerrillis@gmail.com>
2025-04-27 17:11:41 -0400

    Delete api-sniffrpack_auth directory

--- api-sniffrpack_auth/src/auth/routes.js
+++
@@ -1,8 +0,0 @@
-import { register, login, getProfile, logout } from './controllers/authController.js';
-
-export default async function authRoutes(fastify, opts) {
-  fastify.post('/register', register);
-  fastify.post('/login', login);
-  fastify.get('/me', { preHandler: [fastify.authenticate] }, getProfile);
-  fastify.post('/logout', { preHandler: [fastify.authenticate] }, logout);
-};
