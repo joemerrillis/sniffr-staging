3a2aeb99377b9ff3920f74aa955de582e91d69a7
joemerrillis <joemerrillis@gmail.com>
2025-04-28 14:00:47 -0400

    Update routes.js

--- src/users/routes.js
+++ src/users/routes.js
@@ -2,45 +2,74 @@ import { userSchemas } from './schemas/users.js';
 import { list, retrieve, modify, remove } from './controllers/usersController.js';
 
 export default async function usersRoutes(fastify, opts) {
+  // List all users: returns { users: [] }
   fastify.get('/', {
     schema: {
       response: {
         200: {
           type: 'object',
-          properties: { users: { type: 'array', items: userSchemas.User } }
+          properties: {
+            users: { type: 'array', items: userSchemas.User }
+          },
+          required: ['users']
         }
       }
     }
   }, list);
 
+  // Retrieve single user: wrap in { user: {} }
   fastify.get('/:id', {
     schema: {
       params: {
         type: 'object',
-        properties: { id: { type: 'string', format: 'uuid' } },
+        properties: {
+          id: { type: 'string', format: 'uuid' }
+        },
         required: ['id']
       },
-      response: { 200: userSchemas.User }
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            user: userSchemas.User
+          },
+          required: ['user']
+        }
+      }
     }
   }, retrieve);
 
+  // Update user: return { user: {} }
   fastify.patch('/:id', {
     schema: {
       params: {
         type: 'object',
-        properties: { id: { type: 'string', format: 'uuid' } },
+        properties: {
+          id: { type: 'string', format: 'uuid' }
+        },
         required: ['id']
       },
       body: userSchemas.UpdateUser,
-      response: { 200: userSchemas.User }
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            user: userSchemas.User
+          },
+          required: ['user']
+        }
+      }
     }
   }, modify);
 
+  // Delete user: no response schema (204 No Content)
   fastify.delete('/:id', {
     schema: {
       params: {
         type: 'object',
-        properties: { id: { type: 'string', format: 'uuid' } },
+        properties: {
+          id: { type: 'string', format: 'uuid' }
+        },
         required: ['id']
       }
     }
