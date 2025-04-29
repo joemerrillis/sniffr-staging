e580b162838ac0ee3ac8e0776bb676946717bde0
joemerrillis <joemerrillis@gmail.com>
2025-04-28 21:09:34 -0400

    Update routes.js

--- src/tenants/routes.js
+++ src/tenants/routes.js
@@ -1,3 +1,4 @@
+// src/tenants/routes.js
 import { tenantSchemas } from './schemas/tenants.js';
 import {
   list,
@@ -10,35 +11,82 @@ import {
 export default async function tenantsRoutes(fastify, opts) {
   fastify.get('/', {
     schema: {
-      response: { 200: { type: 'object', properties: { tenants: { type: 'array', items: tenantSchemas.Tenant } } } }
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            tenants: {
+              type: 'array',
+              items: tenantSchemas.Tenant
+            }
+          },
+          required: ['tenants']
+        }
+      }
     }
   }, list);
 
   fastify.get('/:id', {
     schema: {
-      params: { type: 'object', properties: { id: { type: 'string', format: 'uuid' } }, required: ['id'] },
-      response: { 200: tenantSchemas.Tenant }
+      params: {
+        type: 'object',
+        properties: { id: { type: 'string', format: 'uuid' } },
+        required: ['id']
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            tenant: tenantSchemas.Tenant
+          },
+          required: ['tenant']
+        }
+      }
     }
   }, retrieve);
 
   fastify.post('/', {
     schema: {
       body: tenantSchemas.CreateTenant,
-      response: { 201: tenantSchemas.Tenant }
+      response: {
+        201: {
+          type: 'object',
+          properties: {
+            tenant: tenantSchemas.Tenant
+          },
+          required: ['tenant']
+        }
+      }
     }
   }, create);
 
   fastify.patch('/:id', {
     schema: {
-      params: { type: 'object', properties: { id: { type: 'string', format: 'uuid' } }, required: ['id'] },
+      params: {
+        type: 'object',
+        properties: { id: { type: 'string', format: 'uuid' } },
+        required: ['id']
+      },
       body: tenantSchemas.UpdateTenant,
-      response: { 200: tenantSchemas.Tenant }
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            tenant: tenantSchemas.Tenant
+          },
+          required: ['tenant']
+        }
+      }
     }
   }, modify);
 
   fastify.delete('/:id', {
     schema: {
-      params: { type: 'object', properties: { id: { type: 'string', format: 'uuid' } }, required: ['id'] }
+      params: {
+        type: 'object',
+        properties: { id: { type: 'string', format: 'uuid' } },
+        required: ['id']
+      }
     }
   }, remove);
 };
