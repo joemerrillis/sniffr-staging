0ac6c2b781cb31bafe3e524a8a608bed38767289
joemerrillis <joemerrillis@gmail.com>
2025-05-05 11:52:53 -0400

    Update routes.js

--- src/dogVisibility/routes.js
+++ src/dogVisibility/routes.js
@@ -1,11 +1,33 @@
 import { dogVisibilitySchemas } from './schemas/dogVisibility.js';
 import {
   getVisibility,
-  updateVisibility
+  createVisibility,
+  updateVisibility,
+  deleteVisibility
 } from './controllers/dogVisibilityController.js';
 
+export default async function visibilityRoutes(fastify, opts) {
+  // Create visibility record for a dog
+  fastify.post('/:id/visibility', {
+    schema: {
+      params: {
+        type: 'object',
+        properties: { id: { type: 'string', format: 'uuid' } },
+        required: ['id']
+      },
+      body: dogVisibilitySchemas.CreateVisibility,     // { is_visible: boolean }
+      response: {
+        201: {
+          type: 'object',
+          properties: {
+            visibility: dogVisibilitySchemas.Visibility  // { dog_id, is_visible }
+          },
+          required: ['visibility']
+        }
+      }
+    }
+  }, createVisibility);
 
- export default async function visibilityRoutes(fastify, opts) {
   // Get current visibility
   fastify.get('/:id/visibility', {
     schema: {
@@ -26,7 +48,7 @@ import {
     }
   }, getVisibility);
 
-  // Toggle visibility
+  // Update (toggle) visibility
   fastify.patch('/:id/visibility', {
     schema: {
       params: {
@@ -34,7 +56,7 @@ import {
         properties: { id: { type: 'string', format: 'uuid' } },
         required: ['id']
       },
-      body: dogVisibilitySchemas.UpdateVisibility,
+      body: dogVisibilitySchemas.UpdateVisibility,     // { is_visible: boolean }
       response: {
         200: {
           type: 'object',
@@ -46,4 +68,15 @@ import {
       }
     }
   }, updateVisibility);
+
+  // Delete visibility record
+  fastify.delete('/:id/visibility', {
+    schema: {
+      params: {
+        type: 'object',
+        properties: { id: { type: 'string', format: 'uuid' } },
+        required: ['id']
+      }
+    }
+  }, deleteVisibility);
 }
