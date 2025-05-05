7e8b3945ba51a8e0f34a6531eafd527f3f16856d
joemerrillis <joemerrillis@gmail.com>
2025-05-05 14:02:57 -0400

    Delete src/dogFriends directory

--- src/dogFriends/index.js
+++
@@ -1,6 +0,0 @@
-import fp from 'fastify-plugin';
-import friendsRoutes from './routes.js';
-
-export default fp(async (fastify) => {
-  fastify.register(friendsRoutes, { prefix: '/dog-friends' });
-});
