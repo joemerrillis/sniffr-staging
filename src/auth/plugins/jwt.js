2f673f6b92b04db0972ba6283ebfe0ef1b17a902
joemerrillis <joemerrillis@gmail.com>
2025-05-06 13:01:02 -0400

    Update jwt.js

--- src/auth/plugins/jwt.js
+++ src/auth/plugins/jwt.js
@@ -1,14 +1,18 @@
 import fp from 'fastify-plugin';
 import fastifyJwt from '@fastify/jwt';
 
-export default fp(async function (fastify, opts) {
+// JWT plugin: registers @fastify/jwt, decorates authenticate, and adds global hook
+export default fp(async function jwtPlugin(fastify, opts) {
   const secret = process.env.JWT_SECRET;
   if (!secret) {
     fastify.log.error('Missing JWT_SECRET');
     throw new Error('JWT_SECRET is required');
   }
+
+  // 1) Register the JWT library
   fastify.register(fastifyJwt, { secret });
 
+  // 2) Decorate Fastify with `authenticate()` to verify JWT on protected routes
   fastify.decorate('authenticate', async function(request, reply) {
     try {
       await request.jwtVerify();
@@ -16,4 +20,7 @@ export default fp(async function (fastify, opts) {
       reply.send(err);
     }
   });
+
+  // 3) Apply the authenticate hook globally on every request after decoration
+  fastify.addHook('onRequest', fastify.authenticate);
 });
