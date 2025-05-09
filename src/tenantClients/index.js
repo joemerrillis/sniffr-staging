eabcc4cae7cf93579cf93f6c405daba6c396b546
joemerrillis <joemerrillis@gmail.com>
2025-05-08 23:10:27 -0400

    Update index.js

--- src/tenantClients/index.js
+++ src/tenantClients/index.js
@@ -1,7 +1,9 @@
+// src/tenantClients/index.js
 import fp from 'fastify-plugin';
 import routes from './routes.js';
 
 // tenantClients plugin: CRUD for the tenant_clients table
+// All routes are mounted under /tenant-clients, avoiding conflicts on '/'
 export default fp(async function tenantClientsModule(fastify, opts) {
-  fastify.register(routes);
+  fastify.register(routes, { prefix: '/tenant-clients' });
 });
