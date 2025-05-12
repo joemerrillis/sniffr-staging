9e0791f2c52b7ca3eb7fa1314433581e14b43393
joemerrillis <joemerrillis@gmail.com>
2025-05-12 16:39:42 -0400

    Delete src/clientWalkWindows directory

--- src/clientWalkWindows/index.js
+++
@@ -1,7 +0,0 @@
-import fp from 'fastify-plugin';
-import routes from './routes.js';
-
-// clientWalkWindows plugin: manage client scheduling windows
-export default fp(async function clientWalkWindows(fastify, opts) {
-  fastify.register(routes, { prefix: '/client-windows' });
-});
