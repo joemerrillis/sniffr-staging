fc2773ced945c40a07bc39df140e146cd6a79e06
joemerrillis <joemerrillis@gmail.com>
2025-05-12 22:56:18 -0400

    Update index.js

--- src/clientWalkWindows/index.js
+++ src/clientWalkWindows/index.js
@@ -1,8 +1,22 @@
 // src/clientWalkWindows/index.js
 import fp from 'fastify-plugin';
 import routes from './routes.js';
+import {
+  ClientWalkWindow,
+  WindowsEnvelope,
+  WindowEnvelope,
+  CreateClientWalkWindow,
+  UpdateClientWalkWindow
+} from './schemas/clientWalkWindowsSchemas.js';
 
-// clientWalkWindows plugin: CRUD for the client_walk_windows table
 export default fp(async function clientWalkWindowsModule(fastify, opts) {
-  fastify.register(routes, { prefix: '/client-windows' });
+  // 1) Make Fastify aware of all our JSON schemas
+  fastify.addSchema(ClientWalkWindow);
+  fastify.addSchema(WindowsEnvelope);
+  fastify.addSchema(WindowEnvelope);
+  fastify.addSchema(CreateClientWalkWindow);
+  fastify.addSchema(UpdateClientWalkWindow);
+
+  // 2) Register our routes (the “prefix” comes from how you mount this plugin in index.js)
+  fastify.register(routes, opts);
 });
