effef6295e8eb4d22270a6eb38bf2c7ee765adbe
joemerrillis <joemerrillis@gmail.com>
2025-05-16 15:05:08 -0400

    Create index.js

---
+++ src/pendingServices/index.js
@@ -0,0 +1,13 @@
+// src/pendingServices/index.js
+import fp from 'fastify-plugin';
+import routes from './routes.js';
+import { PendingService, PendingServicesEnvelope } from './schemas.js';
+
+export default fp(async function pendingServicesPlugin(fastify, opts) {
+  // Register schemas
+  fastify.addSchema(PendingService);
+  fastify.addSchema(PendingServicesEnvelope);
+
+  // Register routes
+  fastify.register(routes);
+});
