bab03e449bb520b5145f389d85ada26a208e8e8f
joemerrillis <joemerrillis@gmail.com>
2025-05-16 15:02:45 -0400

    Create controllers.js

---
+++ src/pendingServices/controllers/controllers.js
@@ -0,0 +1,36 @@
+// src/pendingServices/controllers.js
+
+import {
+  listPendingServices,
+  seedFromWindows,
+  confirmPendingService,
+  deletePendingService
+} from './services.js';
+
+export async function list(req, reply) {
+  const userId = req.user.sub;
+  const { week_start } = req.query;
+  const services = await listPendingServices(req.server, userId, week_start);
+  reply.send({ pending_services: services });
+}
+
+export async function seed(req, reply) {
+  const userId = req.user.sub;
+  const { week_start } = req.query;
+  await seedFromWindows(req.server, userId, week_start);
+  reply.send({ success: true });
+}
+
+export async function confirm(req, reply) {
+  const userId = req.user.sub;
+  const { id } = req.params;
+  const service = await confirmPendingService(req.server, userId, id);
+  reply.send({ pending_service: service });
+}
+
+export async function remove(req, reply) {
+  const userId = req.user.sub;
+  const { id } = req.params;
+  await deletePendingService(req.server, userId, id);
+  reply.code(204).send();
+}
