be2b425171c6085f0fa0e56e597b9c20028614b9
joemerrillis <joemerrillis@gmail.com>
2025-05-19 22:18:00 -0400

    Update index.js

--- src/pendingServices/index.js
+++ src/pendingServices/index.js
@@ -1,4 +1,5 @@
 // src/pendingServices/index.js
+
 import fp from 'fastify-plugin';
 import routes from './routes.js';
 import {
@@ -11,5 +12,5 @@ export default fp(async function pendingServicesPlugin(fastify, opts) {
   fastify.addSchema(PendingService);
   fastify.addSchema(PendingServicesEnvelope);
   fastify.addSchema(PendingServiceEnvelope);
-  fastify.register(routes);
+  fastify.register(routes, opts);
 });
