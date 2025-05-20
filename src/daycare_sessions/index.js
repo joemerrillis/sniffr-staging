53b77d1fe4d4484b97d947c6c3c5fc1fdb7ce6b0
joemerrillis <joemerrillis@gmail.com>
2025-05-20 19:33:05 -0400

    Create index.js

---
+++ src/daycare_sessions/index.js
@@ -0,0 +1,11 @@
+import fp from 'fastify-plugin';
+import routes from './routes.js';
+import { daycareSessionSchemas } from './schemas/daycareSessionsSchemas.js';
+
+export default fp(async function daycareSessionsPlugin(fastify, opts) {
+  // Register schemas (for response validation)
+  fastify.addSchema(daycareSessionSchemas.DaycareSession);
+
+  // Register routes
+  fastify.register(routes, { prefix: '/daycare_sessions' });
+});
