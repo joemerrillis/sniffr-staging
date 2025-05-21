f94d3e5cafc1602e71f0922a391c0d121c733997
joemerrillis <joemerrillis@gmail.com>
2025-05-20 23:21:43 -0400

    Update index.js

--- src/purchases/index.js
+++ src/purchases/index.js
@@ -1,12 +1,8 @@
-// src/purchases/index.js
 import fp from 'fastify-plugin';
 import routes from './routes.js';
 import { purchasesSchemas } from './schemas/purchasesSchemas.js';
 
 export default fp(async function purchasesPlugin(fastify, opts) {
-  // Register schemas for Swagger/OpenAPI
   fastify.addSchema(purchasesSchemas.Purchase);
-  // Register all other schemas as needed
-  // (They don't have $id so not used for $ref, but safe to register)
   fastify.register(routes, { prefix: '/purchases' });
 });
