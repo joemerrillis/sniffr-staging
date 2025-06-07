3e417c02201f81e77a861f822ed5b8e5a2728ab1
joemerrillis <joemerrillis@gmail.com>
2025-06-06 21:24:39 -0400

    Create index.js

---
+++ src/pricingRules/index.js
@@ -0,0 +1,6 @@
+import fp from 'fastify-plugin';
+import pricingRulesRoutes from './routes.js';
+
+export default fp(async function pricingRulesPlugin(fastify, opts) {
+  fastify.register(pricingRulesRoutes, opts);
+});
