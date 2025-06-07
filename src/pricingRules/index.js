843455c8c6406c38ded5c6631b42e334db9977d2
joemerrillis <joemerrillis@gmail.com>
2025-06-06 23:13:56 -0400

    Update index.js

--- src/pricingRules/index.js
+++ src/pricingRules/index.js
@@ -2,5 +2,5 @@ import fp from 'fastify-plugin';
 import pricingRulesRoutes from './routes.js';
 
 export default fp(async function pricingRulesPlugin(fastify, opts) {
-  fastify.register(pricingRulesRoutes, opts);
+  fastify.register(pricingRulesRoutes, { prefix: '/pricing-rules' });
 });
