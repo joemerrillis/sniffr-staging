cf755627cc33feb9a325a90ecfb33103b5710e89
joemerrillis <joemerrillis@gmail.com>
2025-06-07 15:15:16 -0400

    Update index.js

--- src/clientWalkWindows/index.js
+++ src/clientWalkWindows/index.js
@@ -1,12 +1,15 @@
 import fp from 'fastify-plugin';
 import routes from './routes.js';
 import {
+  PricePreview,
   ClientWalkWindow,
   WindowsEnvelope,
   WindowEnvelope
 } from './schemas/clientWalkWindowsSchemas.js';
 
 export default fp(async function (fastify, opts) {
+  // Register PricePreview FIRST so $ref can resolve
+  fastify.addSchema(PricePreview);
   fastify.addSchema(ClientWalkWindow);
   fastify.addSchema(WindowsEnvelope);
   fastify.addSchema(WindowEnvelope);
