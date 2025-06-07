ed2c35360a68cc33eb04d564af9f9956d5cdafef
joemerrillis <joemerrillis@gmail.com>
2025-06-06 21:19:55 -0400

    Update index.js

--- src/boardings/index.js
+++ src/boardings/index.js
@@ -1,31 +1,14 @@
 import fp from 'fastify-plugin';
+import boardingsRoutes from './routes.js';
 
-// Route modules
-import boardingsRoutes from './routes.js'; // This is your "main" boardings route file!
-import pricingRoutes from './routes/pricingRoutes.js';
-import rulesRoutes from './routes/rulesRoutes.js';
-
-// Schemas
 import { boardingSchemas } from './schemas/boardingsSchemas.js';
-import { pricingRulesSchemas } from './schemas/pricingRulesSchemas.js';
 
 export default fp(async function boardingsPlugin(fastify, opts) {
-  // Register core boarding schemas
+  // Register all boardings schemas
   fastify.addSchema(boardingSchemas.Boarding);
   fastify.addSchema(boardingSchemas.CreateBoarding);
   fastify.addSchema(boardingSchemas.UpdateBoarding);
 
-  // Register pricing rules schemas if present
-  if (pricingRulesSchemas) {
-    Object.values(pricingRulesSchemas).forEach(schema => fastify.addSchema(schema));
-  }
-
-  // Main boardings CRUD endpoints
+  // Register just the main boardings CRUD endpoints
   fastify.register(boardingsRoutes, opts);
-
-  // /boardings/pricing
-  fastify.register(pricingRoutes, { prefix: '/pricing' });
-
-  // /boardings/rules
-  fastify.register(rulesRoutes, { prefix: '/rules' });
 });
