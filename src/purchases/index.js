543a718ec637aff46ff79a0a59ee6ebd6b8bbbad
joemerrillis <joemerrillis@gmail.com>
2025-06-08 22:09:48 -0400

    Update index.js

--- src/purchases/index.js
+++ src/purchases/index.js
@@ -1,9 +1,13 @@
 import fp from 'fastify-plugin';
 import routes from './routes.js';
 import { purchasesSchemas } from './schemas/purchasesSchemas.js';
+import { Delegation } from './schemas/delegationSchemas.js'; // <-- ADD THIS
 
 export default fp(async function purchasesPlugin(fastify, opts) {
-  // Only register schemas that have a $id
+  // Register Delegation first, so $ref will resolve!
+  fastify.addSchema(Delegation);
+
+  // Now register all remaining schemas
   for (const key in purchasesSchemas) {
     const schema = purchasesSchemas[key];
     if (schema.$id) {
