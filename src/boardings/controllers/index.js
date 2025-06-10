06bbafc7fbf96411e384a9c7ec98f83c80e46cae
joemerrillis <joemerrillis@gmail.com>
2025-06-10 19:32:51 -0400

    Create index.js

---
+++ src/boardings/controllers/index.js
@@ -0,0 +1,8 @@
+export { default as getUserId } from './getUserId.js';
+export { default as getTenantConfig } from './getTenantConfig.js';
+export { default as validateBlockTimeFields } from './validateBlockTimeFields.js';
+export { default as list } from './list.js';
+export { default as retrieve } from './retrieve.js';
+export { default as create } from './create.js';
+export { default as modify } from './modify.js';
+export { default as remove } from './remove.js';
