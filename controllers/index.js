f3fde1fe282410c36f24ca095b791cd3fa9ced99
joemerrillis <joemerrillis@gmail.com>
2025-06-10 17:10:03 -0400

    Delete controllers directory

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-export { default as createWindow } from './createWindow.js';
-export { default as deleteWindow } from './deleteWindow.js';
-export { default as getWindow } from './getWindow.js';
-export { default as listClientWindowsForTenant } from './listClientWindowsForTenant.js';
-export { default as listWindows } from './listWindows.js';
-export { default as seedWalksForCurrentWeek } from './seedWalksForCurrentWeek.js';
-export { default as updateWindow } from './updateWindow.js';
