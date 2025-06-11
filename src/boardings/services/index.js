63cabdd4d7bfb12bcce90b49272cb776e3bffa69
joemerrillis <joemerrillis@gmail.com>
2025-06-10 21:29:59 -0400

    Create index.js

---
+++ src/boardings/services/index.js
@@ -0,0 +1,6 @@
+export { getDogIdsForBoardings } from './getDogIdsForBoardings.js';
+export { listBoardings } from './listBoardings.js';
+export { getBoarding } from './getBoarding.js';
+export { createBoarding } from './createBoarding.js';
+export { updateBoarding } from './updateBoarding.js';
+export { deleteBoarding } from './deleteBoarding.js';
