c60a447892dbed3f5b2533f76cc26e91fc0021de
joemerrillis <joemerrillis@gmail.com>
2025-06-13 13:24:54 -0400

    Update index.js

--- src/clientWalkRequests/index.js
+++ src/clientWalkRequests/index.js
@@ -4,12 +4,16 @@ import routes from './routes.js';
 import {
   ClientWalkRequest,
   RequestsEnvelope,
-  RequestEnvelope
+  RequestEnvelope,
+  CreateClientWalkRequest,
+  UpdateClientWalkRequest
 } from './schemas/clientWalkRequestsSchemas.js';
 
 export default fp(async function clientWalkRequestsModule(fastify, opts) {
   fastify.addSchema(ClientWalkRequest);
   fastify.addSchema(RequestsEnvelope);
   fastify.addSchema(RequestEnvelope);
+  fastify.addSchema(CreateClientWalkRequest);
+  fastify.addSchema(UpdateClientWalkRequest);
   fastify.register(routes, opts);
 });
