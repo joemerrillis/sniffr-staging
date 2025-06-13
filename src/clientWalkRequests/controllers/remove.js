f45167d8b06adb373cb11758f7905ca9869ca92e
joemerrillis <joemerrillis@gmail.com>
2025-06-13 13:03:58 -0400

    Create remove.js

---
+++ src/clientWalkRequests/controllers/remove.js
@@ -0,0 +1,10 @@
+// controllers/clientWalkRequests/remove.js
+import getUserId from './getUserId.js';
+import { deleteClientWalkRequest } from '../../services/clientWalkRequests/deleteClientWalkRequest.js';
+
+export default async function remove(request, reply) {
+  const userId = getUserId(request);
+  const { id } = request.params;
+  await deleteClientWalkRequest(request.server, userId, id);
+  reply.code(204).send();
+}
