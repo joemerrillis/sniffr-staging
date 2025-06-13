a6b9d30163a81a4e3709fa35a41f2d63f1bc1e32
joemerrillis <joemerrillis@gmail.com>
2025-06-13 13:03:34 -0400

    Create retrieve.js

---
+++ src/clientWalkRequests/controllers/retrieve.js
@@ -0,0 +1,11 @@
+// controllers/clientWalkRequests/retrieve.js
+import getUserId from './getUserId.js';
+import { getClientWalkRequest } from '../../services/clientWalkRequests/getClientWalkRequest.js';
+
+export default async function retrieve(request, reply) {
+  const userId = getUserId(request);
+  const { id } = request.params;
+  const req = await getClientWalkRequest(request.server, userId, id);
+  if (!req) return reply.code(404).send({ error: 'Request not found' });
+  reply.send({ request: req });
+}
