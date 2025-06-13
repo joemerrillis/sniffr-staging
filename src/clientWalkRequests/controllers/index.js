be3db0fbc563b2bcb86aaaf8415aa1067ae88d31
joemerrillis <joemerrillis@gmail.com>
2025-06-13 13:04:11 -0400

    Create index.js

---
+++ src/clientWalkRequests/controllers/index.js
@@ -0,0 +1,14 @@
+// controllers/clientWalkRequests/index.js
+import create from './create.js';
+import list from './list.js';
+import retrieve from './retrieve.js';
+import update from './update.js';
+import remove from './remove.js';
+
+export {
+  create,
+  list,
+  retrieve,
+  update,
+  remove,
+};
