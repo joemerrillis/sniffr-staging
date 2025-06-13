c2f0183fbd0ce0a7b01c01e55d93bbf21eb15151
joemerrillis <joemerrillis@gmail.com>
2025-06-13 13:03:21 -0400

    Create list.js

---
+++ src/clientWalkRequests/controllers/list.js
@@ -0,0 +1,9 @@
+// controllers/clientWalkRequests/list.js
+import getUserId from './getUserId.js';
+import { listClientWalkRequests } from '../../services/clientWalkRequests/listClientWalkRequests.js';
+
+export default async function list(request, reply) {
+  const userId = getUserId(request);
+  const requests = await listClientWalkRequests(request.server, userId);
+  reply.send({ requests });
+}
