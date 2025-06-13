4f7ca5bac6593385c20ecb16f166f691de51fc1c
joemerrillis <joemerrillis@gmail.com>
2025-06-13 13:10:40 -0400

    Create clientWalkRequestsService.js

---
+++ src/clientWalkRequests/services/clientWalkRequestsService.js
@@ -0,0 +1,15 @@
+// src/clientWalkRequests/services/clientWalkRequestsService.js
+
+import listClientWalkRequests from './listClientWalkRequests.js';
+import getClientWalkRequest from './getClientWalkRequest.js';
+import createClientWalkRequest from './createClientWalkRequest.js';
+import updateClientWalkRequest from './updateClientWalkRequest.js';
+import deleteClientWalkRequest from './deleteClientWalkRequest.js';
+
+export {
+  listClientWalkRequests,
+  getClientWalkRequest,
+  createClientWalkRequest,
+  updateClientWalkRequest,
+  deleteClientWalkRequest,
+};
