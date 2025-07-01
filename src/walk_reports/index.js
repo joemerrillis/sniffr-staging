f441a5da9ebda34f6a31af67061ea6def54c0578
joemerrillis <joemerrillis@gmail.com>
2025-07-01 15:21:15 -0400

    Create index.js

---
+++ src/walk_reports/index.js
@@ -0,0 +1,11 @@
+import fp from 'fastify-plugin';
+import walkReportsRoutes from './routes/walkReportsRoutes.js';
+import { walkReportsSchemas } from './schemas/walkReportsSchemas.js';
+
+export default fp(async function walkReportsPlugin(fastify, opts) {
+  // Register all schemas ONCE (avoid duplicate $id errors)
+  for (const schema of Object.values(walkReportsSchemas)) {
+    try { fastify.addSchema(schema); } catch (e) { }
+  }
+  fastify.register(walkReportsRoutes);
+});
