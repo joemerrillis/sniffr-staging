b467cd9cdc686e64726b21449642787484e41e7b
joemerrillis <joemerrillis@gmail.com>
2025-07-02 13:19:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ import dogFriendsPlugin from './src/dogFriends/index.js';
 import visibilityPlugin from './src/dogVisibility/index.js';
 import chatPlugin from './src/chat/index.js';
 import walkReportsPlugin from './src/walk_reports/index.js';
+import dogEventsPlugin from './src/dog_events/index.js';
 
 dotenv.config();
 console.log("REPLICATE_API_TOKEN:", process.env.REPLICATE_API_TOKEN);
@@ -155,6 +156,7 @@ await fastify.register(dogFriendsPlugin, { prefix: '/dog-friends' });
 await fastify.register(visibilityPlugin, { prefix: '/dogs/:id/visibility' });
 await fastify.register(chatPlugin, { prefix: '/chats' });
 await fastify.register(walkReportsPlugin, { prefix: '/walk-reports' });
+await fastify.register(dogEventsPlugin, { prefix: '/dog-events' });
 
 // --- GLOBAL ERROR HANDLER ---
 fastify.setErrorHandler((error, request, reply) => {
