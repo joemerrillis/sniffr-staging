b50406bc00f2a2341fa7335e9636e7e9e5235009
joemerrillis <joemerrillis@gmail.com>
2025-07-04 11:17:03 -0400

    Update createDogEvent.js

--- src/dog_events/controller/createDogEvent.js
+++ src/dog_events/controller/createDogEvent.js
@@ -1,10 +1,14 @@
 import { createDogEvent } from '../service/dogEventService.js';
 
 export async function createDogEventController(request, reply) {
+  const supabase = request.server.supabase;
   try {
-    const event = await createDogEvent(request.body);
-    return reply.code(201).send(event);
+    const event = request.body;
+    console.log('[dog_events] Creating dog event:', event);
+    const created = await createDogEvent(supabase, event);
+    return reply.code(201).send({ event: created });
   } catch (error) {
-    return reply.code(400).send({ error: error.message });
+    console.error('[dog_events] Create error:', error);
+    return reply.code(500).send({ error: error.message });
   }
 }
